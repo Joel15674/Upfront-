0f36d5def541c4cead3752fa732cd595e9f751a7
Joel15674 <pullamatute@gmail.com>
2022-07-10 20:14:27 -0500

    probandoPat

---
+++ src/main/java/com/wissen/Upfront/UpfrontApplication.java
@@ -0,0 +1,13 @@
+package com.wissen.Upfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpfrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpfrontApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wissen/Upfront/UpfrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wissen.Upfront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpfrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
